b45f7c8de91a31fc676f5bfea37f12a39be5e8e8
facingpastactions-png <facingpastactions@gmail.com>
2026-09-12 14:27:56 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Existindo.", // required by Discord API, not shown in the client
         state: "Vigiando o servidor inteiro.",     // this is what people actually see
-        type: 3,               // Custom
+        type: 4,               // Custom
       },
     ],
   },
