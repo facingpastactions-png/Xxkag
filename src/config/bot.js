ce046ed41efbd8d75b210ee32faadb2fdb30a8ca
facingpastactions-png <facingpastactions@gmail.com>
2026-09-12 14:12:05 -0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "Existindo.", // required by Discord API, not shown in the client
         state: "Vigiando o servidor inteiro.",     // this is what people actually see
         type: 3,               // Custom
       },
